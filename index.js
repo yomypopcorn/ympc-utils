c7e448b686cb5e41f3bdb35b499e7363da031cc3
Max Kueng <me@maxkueng.com>
2015-07-31 03:02:11 +0200

    fix indentation

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ var crypto = require('crypto');
 var pad = require('pad');
 
 exports.padTime = function padTime (time) {
-	return pad(16, time, '0');
+  return pad(16, time, '0');
 };
 
 exports.cb = function cb () {
-	var args = Array.prototype.slice.call(arguments);
-	var fn = args.shift();
-	if (typeof fn !== 'function') { return; }
-	fn.apply(null, args);
+  var args = Array.prototype.slice.call(arguments);
+  var fn = args.shift();
+  if (typeof fn !== 'function') { return; }
+  fn.apply(null, args);
 };
 
 // season-independent episode number
 // > S01 E22 = 1022
 exports.sien = function sien (season, episode) {
-	return season * 1000 + episode;
+  return season * 1000 + episode;
 };
 
 exports.removeNonScalars = function (obj) {
-    function replacer (key, value) {
-      if (key && typeof value === 'object') { return undefined; }
-      return value;
-    }
+  function replacer (key, value) {
+    if (key && typeof value === 'object') { return undefined; }
+    return value;
+  }
 
-    return JSON.parse(JSON.stringify(obj, replacer));
+  return JSON.parse(JSON.stringify(obj, replacer));
 };
 
 exports.generateUserToken = function (secret, username) {
